5b8e4ec91ac719ece4b01814a7aa77c7d871541b
Erdem Uncuoğlu <erdem@mvc4.us>
2025-03-10 17:44:44 +0300

    'merge()' method now accepts variable list of arrays.

--- src/Utils/ArrayUtils.php
+++ src/Utils/ArrayUtils.php
@@ -17,32 +17,34 @@ final class ArrayUtils
     }
 
     /**
-     * Improved version of PHP built-in function array_merge_recursive().
-     * Modified to match `array_merge` behavior for numeric keys.
+     * Improved version of PHP built-in function `array_merge_recursive()`.
+     * Modified to match `array_merge()` behavior for numeric keys.
      *
-     * @param array $array1
-     * @param array $array2
+     * @param array ...$arrays
      * @return array
      * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
      * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+     * @link   https://www.php.net/manual/en/function.array-merge-recursive.php#89684
      * @link   https://www.php.net/manual/en/function.array-merge-recursive.php#92195
      * @see    \array_merge()
+     * @see    \array_merge_recursive()
      */
-    public static function merge(array $array1, array $array2): array
+    public static function merge(array ...$arrays): array
     {
-        $merged = $array1;
+        $merged = array_shift($arrays) ?? [];
 
-        foreach ($array2 as $key => $value) {
-            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
-                $value = self::merge($merged[$key], $value);
-            }
-            if (is_string($key)) {
-                $merged[$key] = $value;
-            } else {
-                $merged[] = $value;
+        while ($array = array_shift($arrays)) {
+            foreach ($array as $key => $value) {
+                if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                    $value = self::merge($merged[$key], $value);
+                }
+                if (is_string($key)) {
+                    $merged[$key] = $value;
+                } else {
+                    $merged[] = $value;
+                }
             }
         }
-
         return $merged;
     }
 }
